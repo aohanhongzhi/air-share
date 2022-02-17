92d4d855fbffbe67f9c4c1bb9f2673174fbf0e98
秦时明月 <3028536139@qq.com>
2022-02-17 15:27:18 +0800

    first commit

---
+++ src/test/java/hxy/print/PrintApplicationTests.java
@@ -0,0 +1,13 @@
+package hxy.print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
