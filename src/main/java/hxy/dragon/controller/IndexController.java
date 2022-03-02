fb0b7a7f1b8ede77a0f6127ae0d587eac19a2b23
HOU Xiaoyi (GROW/PJ-PMA) <Xiaoyi.HOU@cn.bosch.com>
2022-03-02 11:48:32 +0800

    模板文件上传

--- src/main/java/hxy/dragon/controller/IndexController.java
+++ src/main/java/hxy/dragon/controller/IndexController.java
@@ -1,7 +1,14 @@
 package hxy.dragon.controller;
 
+import hxy.dragon.dao.model.FileEntity;
+import hxy.dragon.util.file.DirUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -14,5 +21,39 @@ public class IndexController {
     @RequestMapping("/test")
     public String test() {
         return "hello";
+    }
+
+    @RequestMapping("/file")
+    public String file(Model model) {
+        List<FileEntity> list = new ArrayList<>();
+
+        File file = new File(DirUtil.getFileStoreDir());
+        File[] fs = file.listFiles();
+        for (File f : fs) {
+            if (f.isDirectory()) {
+                String name = f.getName();
+                File[] files = f.listFiles();
+                for (File f1 : files) {
+                    String fileName = f1.getName();
+                    String fileUrl = "file/" + name + "/" + fileName;
+                    FileEntity fileEntity = new FileEntity();
+                    if (fileName.startsWith("o_1ft")) {
+                        fileName = fileName.substring(30);
+                    }
+                    fileEntity.setFileName(fileName);
+                    fileEntity.setFileSize(f1.length());
+                    fileEntity.setFilePath(fileUrl);
+                    list.add(fileEntity);
+                }
+            } else {
+                FileEntity fileEntity = new FileEntity();
+                fileEntity.setFileName(f.getName());
+                fileEntity.setFileSize(f.length());
+                list.add(fileEntity);
+            }
+        }
+
+        model.addAttribute("fileList", list);
+        return "file";
     }
 }
