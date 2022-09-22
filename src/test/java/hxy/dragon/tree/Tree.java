4586c65a6da38adaa5782ac70ebbb09ed9dc974b
HouXiaoyi <xiaoyi.hou@cn.bosch.com>
2022-09-22 13:39:33 +0800

    bump up Hikari from 4.0.3 to 5.0.1

---
+++ src/test/java/hxy/dragon/tree/Tree.java
@@ -0,0 +1,19 @@
+package hxy.dragon.tree;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author eric
+ * @program air-share
+ * @description
+ * @date 2022/8/3
+ */
+public class Tree {
+
+    @Test
+    public  void Atest(){
+
+
+
+    }
+}
