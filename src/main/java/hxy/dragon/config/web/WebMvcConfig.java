cf5eaebef91a98f0ccfb28e69b7b2efb7ac44f99
捕风的逍遥侯 <aohanhongzhi@qq.com>
2023-05-31 19:17:04 +0800

    解决参数为"null"的问题

--- src/main/java/hxy/dragon/config/web/WebMvcConfig.java
+++ src/main/java/hxy/dragon/config/web/WebMvcConfig.java
@@ -1,21 +1,34 @@
 package hxy.dragon.config.web;
 
+import hxy.dragon.config.converter.StringToStringConverter;
 import hxy.dragon.util.file.DirUtil;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
- * @author eric
- * @program air-share
- * @description
- * @date 2022/2/24
+ * <p>
+ * MVC通用配置
+ * </p>
+ *
+ * @description: MVC通用配置
+ * @version: V1.0
  */
 @Configuration
-public class ResourcesConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         /** 本地文件上传路径 */
         registry.addResourceHandler("/file/**").addResourceLocations("file:" + DirUtil.getFileStoreDir() + "/");
+    }
+
+    /**
+     * 转换器工厂 addConverterFactory
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToStringConverter());
     }
 }
