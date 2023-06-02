42886965f32cb4f069685babdbb51f257c2fe03c
捕风的逍遥侯 <aohanhongzhi@qq.com>
2023-06-02 21:27:41 +0800

    单元测试完善

--- src/test/java/hxy/dragon/AirShareApplicationTests.java
+++ src/test/java/hxy/dragon/AirShareApplicationTests.java
@@ -1,13 +1,8 @@
 package hxy.dragon;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public abstract class AirShareApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
