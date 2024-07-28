31064eaf2735196e776cd57db4193ed89456b7de
捕风的逍遥侯 <aohanhongzhi@qq.com>
2024-07-28 20:05:02 +0800

    database path

---
+++ src/main/java/hxy/dragon/config/RemoteEnvironmentPostProcessor.java
@@ -0,0 +1,42 @@
+package hxy.dragon.config;
+
+import hxy.dragon.util.AppPath;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * @description bean初始化之前，Spring环境变量修改 https://blog.csdn.net/weixin_43827985/article/details/114368232
+ * 1. 可以读取Azue KeyValue
+ * 2. 可以读取本机的配置文件
+ * <p>
+ * 这个类生效靠：app/src/main/resources/META-INF/spring.factories
+ * @date 2022/2/25
+ */
+@Component
+//@Profile(value = {"prod", "beta"}) // 貌似这个没啥用，其他环境变量的时候也跑了程序
+public class RemoteEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(RemoteEnvironmentPostProcessor.class);
+
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String currentProfile = environment.getProperty("spring.profiles.active");
+
+        final Properties properties = new Properties();
+        String url = "jdbc:sqlite:" + AppPath.getAppPath() + "airshare.db";
+        log.info("数据库目录 {}", url);
+
+        properties.put("spring.datasource.url", url);
+        PropertiesPropertySource propertySource = new PropertiesPropertySource(currentProfile, properties);
+        environment.getPropertySources().addFirst(propertySource);
+
+    }
+}
